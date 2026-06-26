a9c2960368d968fdea4954014f9eff451dd5a58b
Eclypsee <58118247+Eclypsee@users.noreply.github.com>
2026-06-26 14:15:10 -0700

    added disconnect recovery

--- plotter.py
+++ plotter.py
@@ -1,11 +1,18 @@
 import serial
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
+import time 
 
 PORT = 'COM4'
 BAUD_RATE = 115200
 
 MAX_POINTS = 50
+
+frame_id = 0
+frameIDOffset = 0
+
+tnow = 0
+tnowOffset = 0
 
 ser = serial.Serial(PORT, BAUD_RATE, timeout=1)
 
@@ -16,7 +23,7 @@ plt.ion()
 fig, ax = plt.subplots()
 
 ax.set_ylim(150, 1600)
-ax.yaxis.set_major_locator(MultipleLocator(20))
+ax.yaxis.set_major_locator(MultipleLocator(100))
 ax.xaxis.set_major_locator(MultipleLocator(5))
 ax.set_xlabel("Sample")
 ax.set_ylabel("Distance (mm)")
@@ -44,12 +51,12 @@ def get_radar(addr):
 
     return radars[addr]
 
+log_file = open("serial.log", "a", buffering=1)  # line-buffered
+
 while True:
     try:
         line_str = ser.readline().decode(errors="ignore").strip()
-        
-        if line_str:
-            print(line_str, flush=True)
+            
 
         if not line_str or not line_str[0].isdigit():
             continue
@@ -59,18 +66,23 @@ while True:
         # CSV:
         # frame,address,loop_start,retCode,distances,raw_mm,filtered_mm,strength,...
         # trailing comma creates an empty last item, so accept 14 or 13
-        if len(parts) < 13:
+        if len(parts) < 14:
             continue
 
-        frame_id = int(parts[0])
+        frame_id = int(parts[0])+frameIDOffset
+        
         addr = parts[1]
+        
+        retcode = int(parts[3])
+        distances = int(parts[4])
         raw_mm = int(parts[5])
         filtered_mm = int(parts[6])
+        p0_str = int(parts[7])
+        
+        tnow = int(parts[13])+tnowOffset
 
         if raw_mm <= 0 or not (150 <= raw_mm <= 1600):
             text_display.set_text(f"Radar {addr}: No Peak Detected")
-            plt.pause(0.01)
-            continue
 
         radar = get_radar(addr)
 
@@ -98,7 +110,26 @@ while True:
         ax.set_ylim(150, 1600)
 
         plt.pause(0.01)
+        if log_file.tell() == 0:
+            log_file.write("frame id | address | retcode | numDistances | raw_mm | filtered_mm | p0 str | time now")
+        log_file.write(f"{frame_id},{addr},{retcode},{distances},{raw_mm},{filtered_mm},{p0_str},{tnow},\n")
 
+    except serial.SerialException:
+        frameIDOffset=frame_id+1
+        tnowOffset=tnow+1
+        try:
+            ser.close()
+        except Exception:
+            pass
+
+        while True:
+            try:
+                ser = serial.Serial(PORT, BAUD_RATE, timeout=1)
+                ser.reset_input_buffer()
+                print("Reconnected!")
+                break
+            except serial.SerialException:
+                time.sleep(1)
     except KeyboardInterrupt:
         print("Stopped")
         break
